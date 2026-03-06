84f95236f44376f57006367fdfa753c381b873d2
LF27819 <luciafeliperuiz@gmail.com>
2026-03-06 16:07:44 +0100

    Estilos de los componentes principales parecidos a la API

--- AA1-DI-LFR/src/components/Navigation.tsx
+++ AA1-DI-LFR/src/components/Navigation.tsx
@@ -2,23 +2,50 @@ import { NavLink } from "react-router-dom";
 
 export default function Navigation() {
   return (
-    <nav 
-      style={{ 
-        display: "flex", 
+    <nav
+      style={{
+        width: "100%",
+        padding: "18px 0px",
+        display: "flex",
         justifyContent: "center",
-        gap: "50px", 
-        padding: "16px",
-        backgroundColor: "#2c4469" 
+        gap: "40px",
+        flexWrap: "wrap",
+        borderBottom: "1px solid var(--color-borde)",
+        marginBottom:"24px",
       }}
     >
-      <NavLink 
-        to="/">
-        Inicio
+      <NavLink
+        to="/"
+        style={({ isActive }) => ({
+          backgroundColor: isActive
+            ? "var(--color-boton-act)"
+            : "var(--color-boton-des)",
+          color: "var(--color-texto)",
+          padding: "14px 15px",
+          borderRadius: "10px",
+          border: "2px solid var(--color-borde)",
+          fontSize: "1rem",
+          fontWeight: "bold",
+        })}
+      >
+        Cóctel del día 🎲
       </NavLink>
 
       <NavLink
-        to="/cocktails">
-        Cocktails
+        to="/cocktails"
+        style={({ isActive }) => ({
+          backgroundColor: isActive
+            ? "var(--color-boton-act)"
+            : "var(--color-boton-des)",
+          color: "var(--color-texto)",
+          padding: "14px 15px",
+          borderRadius: "10px",
+          border: "2px solid var(--color-borde)",
+          fontSize: "1rem",
+          fontWeight: "bold",
+        })}
+      >
+        Buscar cócteles 🔎
       </NavLink>
     </nav>
   );
